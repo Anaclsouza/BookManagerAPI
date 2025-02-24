745912a9fd63176be86d85ab498041aa86206108
Ana Clara <claranogueiramg@gmail.com>
2025-02-24 19:25:31 -0300

    unit tests

--- src/test/java/com/project/bookmanager/domain/BookManagerServiceTest.java
+++ src/test/java/com/project/bookmanager/domain/BookManagerServiceTest.java
@@ -45,69 +45,67 @@ class BookManagerServiceTest {
         when(bookRepository.findById(1)).thenReturn(Optional.of(bookEntity));
             mockConverterToDomain(book);
             Book responseBook = bookManagerService.getBookById(1);
-
             assertNotNull(responseBook);
             assertEquals(1, book.getId());
             assertEquals("JK Rowling", book.getAuthor());
             assertEquals("A pedra filosofal", book.getTitle());
             assertEquals(1998, book.getYearOfPublication());
-
             verify(bookRepository).findById(1);
     }
 
     @Test
     void getByIdWhenIdNotFound(){
         when(bookRepository.findById(1)).thenReturn(Optional.empty());
-
         BookManagerException exception = assertThrows(
                 BookManagerException.class,
                 () -> bookManagerService.getBookById(1)
         );
-
         assertEquals("Book not found with id: 1", exception.getMessage());
-
         verify(bookRepository, times(1)).findById(1);
     }
 
     @Test
     void getByAllHappyFlow(){
         when(bookRepository.findAll()).thenReturn(Collections.singletonList(bookEntity));
-
         mockConverterToDomain(book);
-
        List<Book> responseBook = bookManagerService.getAllBooks();
-
        assertNotNull(responseBook);
        assertFalse(responseBook.isEmpty());
-
        verify(bookRepository).findAll();
     }
 
     @Test
     void getByAllWhenBookIsNotFound(){
         when(bookRepository.findAll()).thenReturn(new ArrayList<>());
-
         List <Book> responseBook = bookManagerService.getAllBooks();
-
         assertNotNull(responseBook);
         assertTrue(responseBook.isEmpty());
         assertEquals(0,responseBook.size());
-
-
         verify(bookRepository).findAll();
     }
 
-//    TODO: criar tratamento de erro para converter
-//    @Test
-//    void getByAllWhenConverterFailed(){
-//        when(bookRepository.findAll()).thenReturn(Collections.singletonList(bookEntity));
-//
-//        try (MockedStatic<BookConverter> mockedConverter = mockStatic(BookConverter.class)) {
-//            mockedConverter.when(() -> BookConverter.converterToDomain(any(BookEntity.class)))
-//                    .thenReturn(book).;
-//        }
-//
-//    }
+    @Test
+    void deleteHappyFlow(){
+        when(bookRepository.findById(1)).thenReturn(Optional.of(bookEntity));
+        mockConverterToDomain(book);
+         bookManagerService.delete(1);
+        verify(bookRepository, times(1)).delete(bookEntity);
+    }
+
+    @Test
+    void deleteWhenBookIsNotFound() {
+        when(bookRepository.findById(1)).thenReturn(Optional.empty());
+        BookManagerException exception = assertThrows(
+                BookManagerException.class,
+                () -> bookManagerService.delete(1)
+        );
+        assertEquals("book is not found", exception.getMessage());
+        verify(bookRepository, times(1)).findById(1);
+        verify(bookRepository, never()).delete(any());
+    }
+
+
+
 
     public static void mockConverterToDomain( Book book) {
         try (MockedStatic<BookConverter> mockedConverter = mockStatic(BookConverter.class)) {
